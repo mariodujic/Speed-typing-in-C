0985a909d4f05b83b5581c7f66b5214857bbc5c2
Mario Dujić <mario.dujic@gmail.com>
2022-08-18 18:08:29 +0200

    Move functions to appropriate header files

---
+++ definitions.h
@@ -0,0 +1,1 @@
+#define FILE_NAME "dictionary.txt"
--- main.c
+++ main.c
@@ -1,64 +1,7 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-#include <malloc.h>
 #include <time.h>
-
-#define FILE_NAME "dictionary.txt"
-
-int numberOfLinesInFile() {
-    FILE *file = fopen(FILE_NAME, "r");
-    int ch, number_of_lines = 0;
-    do {
-        ch = fgetc(file);
-        if (ch == '\n')
-            number_of_lines++;
-    } while (ch != EOF);
-    if (ch != '\n' && number_of_lines != 0)
-        number_of_lines++;
-    fclose(file);
-    return number_of_lines;
-}
-
-void removeChar(char *str, char garbage) {
-    char *src, *dst;
-    for (src = dst = str; *src != '\0'; src++) {
-        *dst = *src;
-        if (*dst != garbage) dst++;
-    }
-    *dst = '\0';
-}
-
-void writeDictionary(char **dictionary) {
-    char *token;
-    char *search = ":";
-    FILE *file = fopen(FILE_NAME, "r");
-    int length;
-    if (file != NULL) {
-        char line[1500];
-        int index = 0;
-        while (fgets(line, sizeof line, file) != NULL) {
-            token = strtok(line, search);
-            removeChar(token, '"');
-            length = strlen(token);
-            dictionary[index] = malloc((length + 1) * sizeof(char));
-            strcpy(dictionary[index], token);
-            index++;
-        }
-        fclose(file);
-    }
-}
-
-void freeArray(char **dictionary, int size) {
-    for (int i = 0; i < size; i++) {
-        free(&dictionary[i]);
-    }
-    free(dictionary);
-}
-
-int randomNumber(int maxRange) {
-    return rand() % maxRange;
-}
+#include "definitions.h"
+#include "dictionary.h"
+#include "file_reader.h"
 
 int main() {
     time_t t;
@@ -73,6 +16,6 @@ int main() {
         int index = randomNumber(numberOfWords);
         printf("%s", dictionary[index]);
     }
-    freeArray(dictionary, numberOfWords);
+    freeDictionary(dictionary, numberOfWords);
     return 0;
 }
