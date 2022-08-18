918e99268d492e113bbcf79ad4f2c75a0ad3d36c
Mario Dujić <mario.dujic@gmail.com>
2022-08-18 19:19:59 +0200

    Separate definitions and declarations

--- file_reader.h
+++ file_reader.h
@@ -1,16 +1,1 @@
-#include <stdio.h>
-#include "definitions.h"
-
-int numberOfLinesInFile() {
-    FILE *file = fopen(FILE_NAME, "r");
-    int ch, number_of_lines = 0;
-    do {
-        ch = fgetc(file);
-        if (ch == '\n')
-            number_of_lines++;
-    } while (ch != EOF);
-    if (ch != '\n' && number_of_lines != 0)
-        number_of_lines++;
-    fclose(file);
-    return number_of_lines;
-}
+int numberOfLinesInFile();
--- utils.h
+++ utils.h
@@ -1,14 +1,2 @@
-#include <stdlib.h>
-
-void removeChar(char *str, char garbage) {
-    char *src, *dst;
-    for (src = dst = str; *src != '\0'; src++) {
-        *dst = *src;
-        if (*dst != garbage) dst++;
-    }
-    *dst = '\0';
-}
-
-int randomNumber(int maxRange) {
-    return rand() % maxRange;
-}
+void removeChar(char *str, char garbage);
+int randomNumber(int maxRange);
